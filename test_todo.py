dde3354cad48e0ec24de26ec98fa36912a34867e
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-31 21:58:05 +0400

    ref: test_todo.py (pytest-playwright)

--- test_todo.py
+++ test_todo.py
@@ -1,3 +1,18 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture
+def browser_fixture():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        page.close()
+        browser.close()
+
+
 def test_add_todo(page):
     page.goto("https://playwright-todomvc.antonzimaiev.repl.co/#/")
     page.get_by_placeholder("What needs to be done?").click()
