a5c7b69f750da06382d6c4fcda7f02a1389282a6
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-09-01 16:31:23 +0400

    add: conftest.py ref: test_todo.py

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "storage_state": {
+            "cookies": [
+                {
+                    "name": "stepik",
+                    "value": "sd4fFfv!x_cfcstepik",
+                    "url": "https://example.com"  # Замените на нужный URL
+                },
+            ]
+        },
+    }
--- test_todo.py
+++ test_todo.py
@@ -19,3 +19,26 @@ def test_add_todo(page):
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
 
+
+"""
+--headed - playwright по умолчанию запускает браузеры в безголовом(headless) режиме. 
+При использовании данного аргумента, запуск теста произойдет в режиме headed.
+
+--browser - запускать тесты в другом браузере chromium, firefox или webkit. 
+Может быть указано несколько раз (по умолчанию: chromium):
+pytest --headed --browser webkit --browser firefox
+
+--browser-channel - возможно вам потребуется запускать тесты в браузерах  Chrome и Edge,
+ установленных на вашем компьютере:
+pytest --browser-channel=msedge --headed
+
+--slowmo - используется  для замедления выполнения теста на указанное количество миллисекунд:
+pytest --slowmo 1000
+
+--device - можно использовать для имитации поведения браузера для определенного устройства
+В качества параметра передайте поддерживаемый девайс:
+ pytest --device="iPhone 13 Mini"
+ 
+Список поддерживаемых девайсов вы сможете  посмотреть 
+https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/deviceDescriptorsSource.json
+"""
