2c7ebc2a2c1a1b763b249711814017529ceeec00
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-09-03 18:04:36 +0400

    add: pytest.ini ref: test_todo.py, conftest.py

--- conftest.py
+++ conftest.py
@@ -1,17 +1,41 @@
 import pytest
+from playwright.sync_api import sync_playwright
+
+# @pytest.fixture(scope="session")
+# def browser_context_args(browser_context_args):
+#     return {
+#         **browser_context_args,
+#         "storage_state": {
+#             "cookies": [
+#                 {
+#                     "name": "stepik",
+#                     "value": "sd4fFfv!x_cfcstepik",
+#                     "url": "https://example.com"  # Замените на нужный URL
+#                 },
+#             ]
+#         },
+#     }
 
 
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-        **browser_context_args,
-        "storage_state": {
-            "cookies": [
-                {
-                    "name": "stepik",
-                    "value": "sd4fFfv!x_cfcstepik",
-                    "url": "https://example.com"  # Замените на нужный URL
-                },
-            ]
-        },
-    }
+# @pytest.fixture(scope="function")
+# def browser_fixture():
+#     with sync_playwright() as playwright:
+#         browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+#         context = browser.new_context()
+#         page = context.new_page()
+#         yield page
+#         page.close()
+#         browser.close()
+
+@pytest.fixture(scope="function")
+def page(playwright: sync_playwright) -> None:
+    print("\nStarting browser...")
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    context = browser.new_context()
+    page = context.new_page()
+    page.set_viewport_size({"width": 1800, "height": 1200})
+    yield page
+    page.close()
+    context.close()
+    browser.close()
+    print("\nBrowser closed...")
--- test_todo.py
+++ test_todo.py
@@ -1,16 +1,4 @@
 import pytest
-from playwright.sync_api import sync_playwright
-
-
-@pytest.fixture
-def browser_fixture():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        yield page
-        page.close()
-        browser.close()
 
 
 # @pytest.mark.skip_browser("firefox")  # Пропустить тест браузером
